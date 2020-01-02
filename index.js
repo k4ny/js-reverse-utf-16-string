03a9b93f44eb11e621172daf2566642512b24ab1
Jan Kanajlo <kanajlo@jablotron.cz>
2020-01-02 16:27:59 +0100

    feat: working version of reverse function

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+const MIN_HIGH_SURROGATE = '\uD800';
+const MAX_HIGH_SURROGATE = '\uDBFF';
+const MIN_LOW_SURROGATE = '\uDC00';
+const MAX_LOW_SURROGATE = '\uDFFF';
+
 /**
  * Reverses UTF16 string
  * @param {string} source
  * @returns {string}
  */
-module.exports = function reverse(source){
-    return '';
+module.exports = function reverse(source) {
+    const array = source.split('');
+
+    let result = [];
+    for (i = array.length - 1; i >= 0; i--) {
+        if (array[i] >= MIN_LOW_SURROGATE && array[i] <= MAX_LOW_SURROGATE) {
+            result.push(array[i - 1]);
+            result.push(array[i]);
+            i--;
+        } else {
+            result.push(array[i]);
+        }
+    }
+
+    return result.join('');
 }
--- index.test.js
+++ index.test.js
@@ -3,5 +3,21 @@ let reverseUtf16String = require('./index');
 describe('index tests', () => {
     it('should reverse empty string', () => {
         expect(reverseUtf16String('')).toEqual('');
-    });    
+    });
+
+    it('should reverse "a" string', () => {
+        expect(reverseUtf16String('a')).toEqual('a');
+    });
+
+    it('should reverse "𤭢" string', () => {
+        expect(reverseUtf16String('𤭢')).toEqual('𤭢');
+    });
+
+    it('should reverse "abc𤭢" string', () => {
+        expect(reverseUtf16String('abc𤭢')).toEqual('𤭢cba');
+    });
+
+    it('should reverse "𤭢cba" string', () => {
+        expect(reverseUtf16String('𤭢cba')).toEqual('abc𤭢');
+    });
 });
